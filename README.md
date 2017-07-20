6ebc2bb6b9924dff3d835cdcaf62c1c09a975db1
zhouguang <zhouguang@sofyun.com>
2017-07-20 17:59:54 +0800

    add commit1.0.1

--- README.md
+++ README.md
@@ -17,5 +17,20 @@ npm run build
 # build for production and view the bundle analyzer report
 npm run build --report
 ```
+## 说明
 
+``` bash
+# 字体图标规范
+
+字体文件查看assets/iconfont文件夹下
+暂定全部使用font-class形式，特殊多色图标需求再使用symbol,渐变色使用图片，在assets/img路径下
+
+# 公共样式说明
+公共样式在assets/css/common.less文件下查看，效果在页面上公共样式页面可以查看
+严禁更改此文件，特别需求可交流。
+
+# 代码规范
+采用eslint检测插件规范代码，具体参考编写可维护的js这本书籍
+
+```
 For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/pages/test.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="test">
+    <button class="cold-bg">冷色渐变</button>
+    <button class="hot-bg">热色渐变</button>
+    <div class="is-flex test-main">
+      <article class="region">
+      <header>
+        模块一
+      </header>
+      <div class="region-main">
+
+      </div>
+      </article>
+      <article class="region">
+      <header>
+        模块二
+      </header>
+      <div class="region-main">
+
+      </div>
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      show: false
+    };
+  },
+  computed: {
+  },
+  created: function () {
+  },
+  methods: {
+    contralPopup () {
+      if (this.show) {
+        this.show = false;
+      } else {
+        this.show = true;
+      }
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+  .test{
+    .test-main{
+      width: 100%;
+      .region{
+        flex: 1;
+        margin:0 10px;
+      }
+    }
+    button{
+      width: 100px;
+      height: 40px;
+    }
+  }
+</style>
